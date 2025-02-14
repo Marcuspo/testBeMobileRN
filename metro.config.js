f7998d9eb2964b49614c7d93bb0bd345be601cff
Marcus Pontes <marcusporama@gmail.com>
2025-02-14 00:37:51 -0300

    feature: included styledcomponents, react-native-svg

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,22 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    assetExts: [],
+    sourceExts: [],
+  },
+};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig(__dirname);
+  const {assetExts, sourceExts} = defaultConfig.resolver;
+
+  config.resolver.assetExts = assetExts.filter(ext => ext !== 'svg');
+  config.resolver.sourceExts = [...sourceExts, 'svg'];
+
+  return mergeConfig(defaultConfig, config);
+})();
